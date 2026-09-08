89919e3133cf333c571a0442108768a4cc13f0dc
chinookz <github@chinookz.33mail.com>
2026-09-08 18:33:56 +0530

    feat: https://www.reactiongifs.com/r/cnvrtble.gif

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
-import { AuthProvider } from './context/AuthContext';
-import { SystemStatusProvider } from './context/SystemStatusContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AuthProvider>
-      <SystemStatusProvider>
-        <App />
-      </SystemStatusProvider>
-    </AuthProvider>
-  </React.StrictMode>,
+  <img className="fullscreen-image" src="/DHLISBETTER.jpg" alt="" />,
 );
