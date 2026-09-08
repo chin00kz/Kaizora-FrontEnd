aaae7f7423a74139334c088e844c2fa199dff619
chinookz <github@chinookz.33mail.com>
2026-09-08 18:40:06 +0530

    feat: 4 RS & TPL14

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <img className="fullscreen-image" src="/DHLISBETTER.jpg" alt="" />,
+  <a
+    className="fullscreen-link"
+    href="https://www.reactiongifs.com/r/cnvrtble.gif"
+    target="_blank"
+    rel="noreferrer"
+  >
+    <img className="fullscreen-image" src="/DHLISBETTER.jpg" alt="" />
+  </a>,
 );
